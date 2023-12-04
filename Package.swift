724ccfc11391704fddfe637431dbe61e3eca3342
Nicola Ferruzzi <nicola.ferruzzi+github@gmail.com>
2023-12-04 17:40:51 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/nferruzzi/openssl/releases/download/0.0.6/openssl_1_1_1q.xcframework.zip",
+            url: "https://github.com/nferruzzi/openssl/releases/download/0.0.7/openssl_1_1_1q.xcframework.zip",
             checksum: "17362dec226e1ec1d49d8a2402c4076769d8ad91ff7c52fbf9abcd8940411d3b"),
         .target(name: "OpenSSL",
                 dependencies: ["openssl"])
